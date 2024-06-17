d15ba7a348951db6f4dc3b03b51e9b9ff85cb848
최명식 <audtlr93@naver.com>
2024-06-17 15:21:42 +0900

    [Structure] 코드 아키텍처 베이스

---
+++ FRONTEND/src/common/common.js
@@ -0,0 +1,25 @@
+/**
+ * Write common util code like 
+ * Validation.js
+ * Connect.js
+ */
+
+$(document).ready(function () {
+    $("header").append(`
+        <div class="header-top">
+            Hader Logo And ...
+        </div>
+        <div class="header-nav">
+            Header Nav ...
+        </div>
+    `);
+
+    $("footer").append(`
+        <div class="footer-top">
+            Footer Top ...
+        </div>
+        <div class="footer-bot">
+            Footer Bot ...
+        </div>
+    `);
+});
