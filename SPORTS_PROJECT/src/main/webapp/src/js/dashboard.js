9f0ba1d5e3306f0d264cb182d0b276dcc9a9d290
최명식 <audtlr93@naver.com>
2024-07-09 17:57:16 +0900

    [Design] 대시보드 데이터화 작업중

--- SPORTS_PROJECT/src/main/webapp/src/js/dashboard.js
+++ SPORTS_PROJECT/src/main/webapp/src/js/dashboard.js
@@ -3,6 +3,83 @@ let year;
 let date;
 let day;
 
+let currentData = [
+	{homeTeam : "한화", homeScore : 6, inning : 10, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "기아", homeScore : 5, inning : 7, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "다저스", homeScore : 6, inning : 3, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "공군", homeScore : 4, inning : 7, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "삼성", homeScore : 2, inning : 7, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "쌍방울", homeScore : 6, inning : 4, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "롯데", homeScore : 1, inning : 7, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "한화", homeScore : 6, inning : 7, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "한화", homeScore : 6, inning : 7, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "한화", homeScore : 3, inning : 5, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "한화", homeScore : 6, inning : 7, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "한화", homeScore : 6, inning : 1, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "한화", homeScore : 7, inning : 7, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "한화", homeScore : 6, inning : 7, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "한화", homeScore : 6, inning : 7, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "한화", homeScore : 6, inning : 7, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "한화", homeScore : 6, inning : 7, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "한화", homeScore : 6, inning : 7, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "한화", homeScore : 6, inning : 7, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "한화", homeScore : 6, inning : 7, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "한화", homeScore : 6, inning : 7, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+	{homeTeam : "한화", homeScore : 6, inning : 7, awayTeam : "기아", awayScore : 8, date : "2024-07-09"},
+]
+
+$(document).ready(function (){
+	displayCurrentEvent(currentData)
+})
+
+function displayCurrentEvent(data){
+	let html = "";
+	
+	
+	// 1. 데이터를 7개씩 나눔
+	// 2. for문 돌려서 각 데이터마다 하나의 ul 로 만듬
+	// 3. html 로 붙임
+	
+	const lists = []
+	
+	for (let i = 0; i < data.length; i += 7){
+		lists.push(data.slice(i, i + 7));
+	}
+	
+	for (let list of lists){
+		let innerHtml = '<ul class="swiper-slide card__white">'
+		
+		for(let d of list){
+			innerHtml += `
+				<li class="d-flex right-win">
+		            <div class="score-left-symbol score__red"></div>
+			            <span class="score-left-team fs-14"> ${d.homeTeam} </span>
+			            <span class="score-left-score fs-14__b"> ${d.homeScore} </span>
+	                     
+	                    ${d.inning == 10 ? 
+	         				`<div class="score-inning fs-20">
+		                        종료
+		                    </div>` 
+	     					: 
+				          `<div class="score-inning fs-20">
+		                        ${d.inning}회<div class="score-inning-up"></div>
+		                    </div>`
+						}
+						         
+			            <span class="score-right-score fs-14__b"> ${d.awayScore} </span>
+			            <span class="score-right-team fs-14"> ${d.awayTeam} </span>
+		            <div class="score-right-symbol score__green"></div>
+		        </li>
+				`	
+			}
+		innerHtml += '</ul>'
+		
+		html += innerHtml
+	}
+
+	$(".current-events-score").html(html)
+}
+
 var swiper = new Swiper(".swiper-events", {
     spaceBetween: 30,
     centeredSlides: false,
@@ -13,10 +90,6 @@ var swiper = new Swiper(".swiper-events", {
     pagination: {
     el: ".swiper-pagination",
     clickable: true
-    },
-    navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
     }
 });
 
@@ -32,10 +105,6 @@ var swiper2 = new Swiper(".swiper-news", {
     pagination: {
     el: ".swiper-pagination",
     clickable: true
-    },
-    navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
     }
 });
 
@@ -51,10 +120,6 @@ var swiper3 = new Swiper(".swiper-highlight", {
     pagination: {
     el: ".swiper-pagination",
     clickable: true
-    },
-    navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
     }
 });
 
