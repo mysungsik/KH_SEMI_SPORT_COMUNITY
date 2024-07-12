edee3d59a6d4c26c3b0881c9ef7a60f21e548af3
최명식 <audtlr93@naver.com>
2024-07-12 11:25:53 +0900

    [Fix] Util 객체 생성  - XSS 방지  -  newLine 핸들링

---
+++ SPORTS_PROJECT/src/main/java/com/helpgpt/sports/common/util/Util.java
@@ -0,0 +1,18 @@
+package com.helpgpt.sports.common.util;
+
+public class Util {
+	public static String newLineHandling(String text) {
+		// 엔터로 담긴 텍스트 <br> 로 변경하여 처리
+		return text.replaceAll("(\n|\r|\r\n|\n\r)", "<br>");
+	}
+
+	public static String XSSHandling(String text){
+		// <, >, &, " 문자를 HTML 코드가 아닌 문자 그대로 보이도록 변경
+		text = text.replaceAll("&", "&amp;");
+		text = text.replaceAll("<", "&lt;");
+		text = text.replaceAll(">", "&gt;");
+		text = text.replaceAll("\"", "&quot;");
+		
+		return text;
+	}	
+}
