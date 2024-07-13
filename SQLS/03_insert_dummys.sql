44f0b6decbbe17fddfd3b6303cead49a3ffaee44
최명식 <audtlr93@naver.com>
2024-07-14 00:41:18 +0900

    [Docs] SQL 파일 추가  - USER_INFO / REPROT  - 더미데이터 추가 작업중

---
+++ SQLS/03_insert_dummys.sql
@@ -0,0 +1,16 @@
+-- [[ INDEX ]]
+-- 1. USER_INFO TABLE
+-- 2. USER HISTORY TABLE
+-- 3. USER_POLICY TABLE
+-- 4. REPORT TYPE TABLE
+-- 5. REPORT VIOLATION TYPE TABLE
+-- 6. REPORT TABLE
+
+-- ========================================================================================================
+
+-- 1. USER TABLE
+-- 2. USER HISTORY TABLE
+-- 3. USER_POLICY TABLE
+-- 4. REPORT TYPE TABLE
+-- 5. REPORT VIOLATION TYPE TABLE
+-- 6. REPORT TABLE
